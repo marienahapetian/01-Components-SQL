2796305e464c3871458e710d60af2a53c5db29f1
Marie <marie.nahapetian@gmail.com>
2026-02-05 16:34:25 +0100

    add swagger doc for api

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const filmsRoutes = require("./routes/films");
 const authRoutes = require("./routes/auth");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 app.use(express.json());
-app.use("/films", filmsRoutes);
+app.use("/api/films", filmsRoutes);
 app.use("/auth", authRoutes);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => {
 	console.log("Serveur démarré sur http://localhost:3000");
